fc2470bcb27c1e9e54492eee995c0e901468d0d1
hojin.joseph.choi <escape3rd@gmail.com>
2018-09-14 22:12:55 +0900

    node-fetch -> axios 플러그인 변경 및 open api 항목 추가

--- graphql/db.js
+++ graphql/db.js
@@ -1,20 +1,42 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
-const API_URL = 'https://yts.am/api/v2/list_movies.json?';
+const BASE_URL = 'https://yts.am/api/v2/';
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = (limit, rating) => {
-  let REQUEST_URL = API_URL;
-  
-  const queryStrings = [];
-  if (limit > 0) {
-    queryStrings.push(`limit=${limit}`);
-  }
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  });
+  return movies;
+};
 
-  if (rating > 0) {
-    queryStrings.push(`minimum_rating=${rating}`);
-  }
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
 
-  return fetch(`${API_URL}` + queryStrings.join('&'))
-    .then(res => res.json())
-    .then(json => json.data.movies);
+export const getSuggestions = async (id) => {
+  const json = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return json.data.data.movies;
 };
